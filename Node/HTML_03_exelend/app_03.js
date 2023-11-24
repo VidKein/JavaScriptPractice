1fd11b2e92c24fea3eb50318128d184a91fdc237
VidKein <landdru@ukr.net>
2023-11-24 08:51:33 +0100

    node translate

---
+++ Node/HTML_03_exelend/app_03.js
@@ -0,0 +1,60 @@
+//создаем метод для работы с адресом
+const http = require('http');
+const PORT = 3500;
+//создаем метод для работы с файдами
+const fs = require('fs');
+//Работа с портом
+const path = require('path');
+//Список констант для работы с загаловками
+const mimeTypes = {
+    '.html': 'text/html',
+    '.js':'text/javascript',
+    '.css':'text/css',
+    '.json':'application/json',
+    '.png':'image/png',
+    '.jpg':'image/jpeg',
+    '.gif':'image/gif',
+    '.ttf':'font/ttf',
+    '.svg':'image/svg+xml',
+    '.eot':'application/vnd.ms-fontobject',
+};
+//Функция для подготовки запроса
+function saticFile(res, filePath, ext) {//респонс от сервера+путь к файлу+его расширение
+    res.setHeader("Content-Type", mimeTypes[ext]);
+    //Асихронная аперация
+    fs.readFile('./Node/HTML_03_exelend/page'+filePath, (error,data)=>{
+        if (error) {
+            res.statusCode = 404;
+            res.end();
+        }else{
+           res.end(data); 
+        }
+    });
+
+}
+
+//прослушыаем порт 3500
+//Locakhost:3500
+//Функция обрабатывает запро и дает ответ
+//request, response
+http.createServer(function (req, res) {
+    //следим зв адресом
+    const url = req.url;
+    console.log(url);  
+    switch (url) {
+        case "/": 
+        saticFile(res,'/translate.html','.html');
+        break;
+
+        default:
+            //считываем расширение
+            const extName = String(path.extname(url)).toLowerCase();
+            if (extName in mimeTypes) {
+                saticFile(res, url, extName)
+            } else {
+                res.statusCode = 404;
+                res.end();
+            }
+        break;
+    }  
+}).listen(PORT);
---
+++ Node/HTML_03_exelend/page/js/script.js
@@ -0,0 +1,51 @@
+// Выбираем язык браузера пользователя
+let language = window.navigator.language;
+let languageFistTwo = language.substr(0,2); 
+//Список языков которые использует язык
+let langList =["en","ru","uk"];
+//Управляем изменениями языка сайта
+let currentLocation = document.getElementsByTagName('html')[0];
+console.log(currentLocation);
+window.onload = ()=>{ 
+    //Устанавливаем язык переключателя
+    if(langList.some(test => test === languageFistTwo)){
+        currentLocation.setAttribute('lang', languageFistTwo);
+        document.getElementById(languageFistTwo).className="active";
+        console.log("Язык - "+languageFistTwo);
+    }else{
+        //меняем локализацию сайта
+        currentLocation.setAttribute('lang', "en");
+        document.getElementById("en").className="active";
+    }
+}
+//Открываем / закрываем меню языка
+    let languageSelect = document.getElementById("languageSelect");
+    languageSelect.addEventListener("click",(e)=>{
+        if(e.currentTarget.className == "languageSelect open"){
+            e.currentTarget.setAttribute('class', "languageSelect");
+        }else{
+            e.currentTarget.className += " open";  
+        };
+    })
+    
+//Выбираем язык
+//Выбираем переключатель языка
+let le = document.getElementsByTagName("li");
+let markerLang;
+for (let i = 0; i < le.length; i++) {
+        le[i].addEventListener("click", (e)=>{
+            for(let langBloc of le){langBloc.removeAttribute("class");}
+            e.target.className = "active";
+            markerLang = e.target.id;
+        })
+}
+
+//Переключение картинок
+let links = document.getElementsByClassName("links");
+let content = document.getElementsByClassName("content");
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", (e)=>{
+    for(let picturBloc of content){picturBloc.style = "display: none;";}
+    document.getElementById(e.currentTarget.name).style = "display: flex;";    
+    })
+}
