853557acf12f3b23981dceeca7044b0a416e8ccb
VidKein <landdru@ukr.net>
2023-12-03 21:04:25 +0100

    node+json

--- Node/translate_node_json/app.js
+++ Node/translate_node_json/app.js
@@ -2,6 +2,8 @@ const http = require('http');
 const url = require('url');
 //создаем метод для работы с файдами
 const fs = require('fs');
+//Работа с портом
+const path = require('path');
 
 //Переменная для парсинга боди POST запроса
 const {parse} = require('querystring');
@@ -9,7 +11,24 @@ const {parse} = require('querystring');
 http.createServer((req, res)=>{
     //следим за адресом
     const url = req.url;
+    //Выводим картинку
             if (req.method == "GET") {
+                if(url.includes("img")){
+                    //Список констант для работы с загаловками
+                    const mimeTypes = {
+                        '.png':'image/png',
+                        '.jpg':'image/jpeg',
+                        '.gif':'image/gif',
+                        '.ttf':'font/ttf',
+                        '.svg':'image/svg+xml',
+                    };
+                    const extName = String(path.extname(url)).toLowerCase();
+                    if (extName in mimeTypes) {
+                        res.setHeader("ContentType",mimeTypes[extName]);
+                    }
+                    let img = fs.readFileSync('./page'+url);
+                    res.write(img);
+                }
                 //Отсылаю файл (СИНХРОННЫЙ ЗАПРОС )
                 let data = fs.readFileSync('./page/index.html',{encoding : 'utf8', flag:"r"});
                 res.write(data);
@@ -31,5 +50,7 @@ http.createServer((req, res)=>{
                     res.end(langEng);
                 });
             }     
+
+
 
 }).listen(3500);
